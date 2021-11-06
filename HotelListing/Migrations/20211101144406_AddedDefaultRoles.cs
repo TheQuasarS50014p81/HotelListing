67484d768304884e53f3840cb9d51eef265f6f8f
The Quasar <20722202+TheQuasarS50014p81@users.noreply.github.com>
2021-11-07 00:38:18 +0900

    implement register

---
+++ HotelListing/Migrations/20211101144406_AddedDefaultRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class AddedDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "20e9a3ce-0ce3-4084-86a2-1ef93178c733", "7b923cfd-23c5-4a60-b50a-a1dcd717b880", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "fdc6d808-70cb-4c68-8dd7-ac870f8c1b1e", "b29b128f-392b-4120-930e-7287e2a72d13", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "20e9a3ce-0ce3-4084-86a2-1ef93178c733");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fdc6d808-70cb-4c68-8dd7-ac870f8c1b1e");
+        }
+    }
+}
