d711810764d6aa7f4b3e30d6ea6a8f2205733d09
The Quasar <20722202+TheQuasarS50014p81@users.noreply.github.com>
2021-11-21 22:10:52 +0900

    Added Value Added Features to file API (Versioning, Caching, paging, Thwolting)

--- HotelListing/IRepository/IGenericRepository.cs
+++ HotelListing/IRepository/IGenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using HotelListing.Models;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,17 @@ namespace HotelListing.IRepository
     {
         Task<IList<T>> GetAll(
             Expression<Func<T, bool>> expression = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy =null,
-            List<string> includes = null
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null
         );
 
         Task<IPagedList<T>> GetPageList(
             RequestParams requestParams,
-            List<String> includes = null
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null
             );
 
-        Task<T> Get(Expression<Func<T, bool>> expression, List<String> includes = null);
+        Task<T> Get(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null
+);
         Task Insert(T entity);
         Task InsertRange(IEnumerable<T> entities);
         Task Delete(int id);
